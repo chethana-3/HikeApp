c1b78910753585eb99fc84a2729ba00fc0116716
Chethana NP <chethana0599@gmail.com>
2024-06-10 23:06:36 +0530

    Initial commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Chethana N P on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
